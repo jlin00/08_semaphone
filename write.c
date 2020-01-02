f2c8fde0e9667e5d454495a9622b0b2eaea95569
Jackie Lin <jlin00@stuy.edu>
2020-01-01 19:18:49 -0500

    added dummy code

---
+++ write.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/shm.h>
+
+#define MEM_KEY 24602
+#define SEM_KEY 10282
+
+int main() {
+  return 0;
+}
